defddb8fe5becd46b6adc4873a61e6e77bb64c09
Jesus <jesusarcos_232@hotmail.com>
2024-01-16 03:38:17 +0100

    Actualizar Sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,13 @@
 plugins {
     id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id ("org.sonarqube") version "4.4.1.3373"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "JesusArcosMurillo_PracticaGPS")
+        property("sonar.organization", "jesusarcosmurillo")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
